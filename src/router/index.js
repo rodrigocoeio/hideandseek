64c24fc213cbc472dc3212a92e5170b59fae89dc
Rodrigo Coelho <rodrigocoeio@gmail.com>
2023-02-16 17:46:48 -0300

    Fixed router index

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: import('../views/HideAndSeekView.vue')
+      component: () => import('../views/HideAndSeekView.vue')
     }
   ]
 })
